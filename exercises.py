198b41213c84e392d309a2bd35bb97c24b79ebb2
redavmok <you@example.com>
2023-02-13 11:05:55 +0800

    approved

--- exercises.py
+++ exercises.py
@@ -61,10 +61,10 @@ while True:
 print('Done!')
 print(maximum, minimum)
 
-# largest = None
-# print('Before:', largest)
-# for itervar in [3, 41, 12, 9, 74, 15]:
-#     if largest is None or itervar > largest :
-#         largest = itervar
-#     print('Loop:', itervar, largest)
-# print('Largest:', largest)
+largest = None
+print('Before:', largest)
+for itervar in [3, 41, 12, 9, 74, 15]:
+    if largest is None or itervar > largest :
+        largest = itervar
+    print('Loop:', itervar, largest)
+print('Largest:', largest)
