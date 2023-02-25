93792df617bf37c7ac9164464a38c58bd346988e
Davis Burke <davisburke@gmail.com>
2023-02-25 19:31:30 +0800

    s

--- PY4E/ex92.py
+++ PY4E/ex92.py
@@ -1,9 +1,13 @@
+# this prints a list of the days emails are printed from, and counts how many
+
 file_name = input('Enter name: ')
 if len(file_name) < 1: file_name = 'mbox-short.txt'
 handle = open(file_name)
 
+# this block creates a dictionary to go line by line through the file
 di = dict()
 for line in handle:
+# this if statement finds the email addresses, and skips the line if it's not the start of an email   
     if not line.startswith("From "):
         continue
     else:
